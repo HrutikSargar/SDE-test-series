6cc6bbcc83c4faf53519368d00c0cbe539206d54
Hrutik Sargar <106005765+HrutikSargar@users.noreply.github.com>
2024-07-21 23:36:45 +0530

    Update 1_replace0'sWith5.java

--- 1.Array/1_replace0'sWith5.java
+++ 1.Array/1_replace0'sWith5.java
@@ -7,7 +7,7 @@ Example 1:
 Input:
 N = 1004
 Output: 1554
-Explanation: There are two zeroes in 1004
+Explanation: There are two zeroes in 1004.
 on replacing all zeroes with "5", the new
 number will be "1554".
 Example 2:
